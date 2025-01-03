024d30bae64ccb638b62ed764f0f32990853d398
Felipe Fernandes <69127022+felipeffsa@users.noreply.github.com>
2025-01-03 09:48:56 -0300

    Update main.py
    Apenas foi adicionada uma descrição no arquivo "main.py"

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Esse arquivo é somente para testar linhas de códigos
+
 # Como utilizar um if e else no list comprehension
 numeros = [numero*2 if numero>3 else "nada" for numero in range(1,11)]
 
